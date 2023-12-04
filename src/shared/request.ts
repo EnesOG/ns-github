c9d3f02782aa2c591ef32325b48b5848b34e15c1
EnesOG <sagir_enes@hotmail.com>
2023-12-04 10:21:26 +0100

    updated function

--- src/shared/request.ts
+++ src/shared/request.ts
@@ -1,9 +1,6 @@
 import { baseApiUrl } from "./helpers";
 
-async function request<T = Response>(
-  input: RequestInfo,
-  init?: RequestInit | undefined
-) {
+async function request<T = Response>(input: RequestInfo, init?: RequestInit) {
   let response = await fetch(baseApiUrl(input), init);
   if (!response.ok) return Promise.reject(response.json());
   return response.json() as T;
